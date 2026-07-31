a463e97de3818196bf832de2eca10be4bb0e7612
Favour Ejakpevweoghene Onosemuode <onosejas@gmail.com>
2026-07-31 08:01:46 +0100

    escaped the supabase auth

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
-import { updateSession } from '@/lib/supabase/proxy'
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  return NextResponse.next()
 }
+
+// import { updateSession } from '@/lib/supab/ase/proxy'
+// import { type NextRequest } from 'next/server'
+
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request)
+// }
 
 export const config = {
   matcher: [
